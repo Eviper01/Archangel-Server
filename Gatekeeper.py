4f388f748a09d466a80dbb85f381ed6c7bcda521
John Wallis <eviper01@gmail.com>
2020-07-23 16:38:48 +0800

    bluetooth pre 1.2 - GPIO Control

--- Gatekeeper.py
+++ Gatekeeper.py
@@ -1,4 +1,9 @@
 import bluetooth
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(14,GPIO.OUT)
+GPIO.setup(15,GPIO.OUT)
 server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 port = 1
 server_sock.bind(("",port))
@@ -16,8 +21,13 @@ while True:
 
 client_sock.close()
 server_sock.close()
+GPIO.cleanup()
 #need to restart the program when the connection drops
 
 def doorOpen():
-    #do shit with the gpio
-    pass
+    GPIO.output(14,True)
+    time.sleep(.5)
+    GPIO.output(15,True)
+    time.sleep(1)
+    GPIO.output(14,False)
+    GPIO.output(15,False)
